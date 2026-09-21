bfb19ad6668643dfd09666a0a46e6c058723ddad
Olivier Houte <github@houte.com>
2026-09-21 17:44:28 +0200

    Add synchronized video export with demo audio

--- dck/main.go
+++ dck/main.go
@@ -15,7 +15,7 @@ import (
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/audio"
+	audio "github.com/olivierh59500/democonstructionkit/sound/output"
 )
 
 const (
